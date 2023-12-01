f88b825c6c1fa4c874d0c338ffe7bebb6ebebe69
Cody Logan <cody@lokken.dev>
2023-12-01 09:31:12 -0800

    Update Python library formulae
    Closes #8.
    
    Signed-off-by: Cody Logan <cody@lokken.dev>

--- Formula/tqdm.rb
+++ Formula/tqdm.rb
@@ -1,6 +1,4 @@
 class Tqdm < Formula
-  include Language::Python::Virtualenv
-
   desc "Fast, extensible progress bar for Python and CLI"
   homepage "https://tqdm.github.io/"
   url "https://files.pythonhosted.org/packages/62/06/d5604a70d160f6a6ca5fd2ba25597c24abd5c5ca5f437263d177ac242308/tqdm-4.66.1.tar.gz"
@@ -9,15 +7,24 @@ class Tqdm < Formula
   head "https://github.com/tqdm/tqdm.git", branch: "master"
 
   depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
 
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
   def install
-    python_exe = Formula["python@3.12"].opt_libexec/"bin/python"
-    system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
   end
 
   test do
-    python_exe = Formula["python@3.12"].opt_libexec/"bin/python"
-    system python_exe, "-c", "import tqdm"
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import tqdm"
+    end
   end
 end
