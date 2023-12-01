adae7e6fdc44baa34d1848cb3168d15064ead257
Cody Logan <cody@lokken.dev>
2023-12-01 09:31:12 -0800

    Update Python library formulae

--- Formula/mwclient.rb
+++ Formula/mwclient.rb
@@ -1,6 +1,4 @@
 class Mwclient < Formula
-  include Language::Python::Virtualenv
-
   desc "Python client library to interface with the MediaWiki API"
   homepage "https://github.com/mwclient/mwclient"
   url "https://files.pythonhosted.org/packages/97/b4/5fc70ad3286a8d8ec4b9ac01acad0f6b00c5a48d4a16b9d3be6519b7eb21/mwclient-0.10.1.tar.gz"
@@ -9,17 +7,26 @@ class Mwclient < Formula
   head "https://github.com/mwclient/mwclient.git", branch: "master"
 
   depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
   depends_on "python-requests-oauthlib"
   depends_on "six"
 
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
   def install
-    python_exe = Formula["python@3.12"].opt_libexec/"bin/python"
-    system python_exe, *Language::Python.setup_install_args(prefix, python_exe)
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, *Language::Python.setup_install_args(prefix, python_exe)
+    end
   end
 
   test do
-    python_exe = Formula["python@3.12"].opt_libexec/"bin/python"
-    system python_exe, "-c", "import mwclient"
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import mwclient"
+    end
   end
 end
