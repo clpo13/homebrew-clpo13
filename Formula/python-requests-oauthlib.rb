6b4cd2508a81a5e2c838ea5325be8dfe8117df3d
Cody Logan <cody@lokken.dev>
2024-03-05 18:10:01 +0000

    python-requests-oauthlib: add 1.3.1 bottle.

--- Formula/python-requests-oauthlib.rb
+++ Formula/python-requests-oauthlib.rb
@@ -5,6 +5,11 @@ class PythonRequestsOauthlib < Formula
   sha256 "75beac4a47881eeb94d5ea5d6ad31ef88856affe2332b9aafb52c6452ccf0d7a"
   license "ISC"
 
+  bottle do
+    root_url "https://ghcr.io/v2/clpo13/clpo13"
+    sha256 cellar: :any_skip_relocation, ventura: "818decd1bef88e5c2bd35d01f605aca5455400abd0b43edc8f52baeef80c2dee"
+  end
+
   depends_on "python-setuptools" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
